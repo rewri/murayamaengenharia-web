6b87bba9e634625d1022670ec688099081715655
Rérisson Fumes <rkfumes@gmail.com>
2026-04-30 16:35:47 -0300

    fix: correções diversas

--- src/config/portfolio/vale_do_sol/config.ts
+++ src/config/portfolio/vale_do_sol/config.ts
@@ -10,8 +10,6 @@ export const valeDolSol = [
     client: "Amando de Barros",
     conclusion_year: 2026,
     services: ["Projeto", "Gerenciamento", "Acompanhamento"],
-    imageCount: 4,
-    video:
-      '<iframe width="560" height="315" src="https://www.youtube.com/embed/9mxj3h27fu8?si=eczMsfKHm7nYYyYX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+    video: null,
   },
 ];
